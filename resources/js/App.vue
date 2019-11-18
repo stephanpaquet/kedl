3bd6e0bf99da71e9470d7740de6d4229f41c7b66
Stéphan Paquet <stephan.paquet@imarcom.net>
2019-11-18 13:07:11 -0500

    - Installation de Laravel 5.8.35 - Installation et configuration de vuetify - Ajout de .idea et public dans le .gitignore

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app>
+        <v-alert :value="true" type="success">
+            You have successfully installed vuetify !
+        </v-alert>
+    </v-app>
+</template>
+
+<script>
+  export default {
+    name: "Vue"
+  }
+</script>
