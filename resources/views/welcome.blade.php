57a0b998b486a4a5fc1fa463d5dd5647041d67df
Stéphan Paquet <stephan.paquet@imarcom.net>
2019-11-21 14:09:01 -0500

    Add Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
-        <title>Kedl</title>
-    </head>
-    <body>
+@extends('layouts.app')
+
+@section('content')
         <div id="app">
 
         </div>
+@endsection
 
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>
-</html>
+
